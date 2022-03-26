face22f6ab97a23d95cea91f52314523bef21628
NyergesC <cintia.nyerges@gmail.com>
2022-03-26 04:45:23 +0100

    backend

---
+++ frontend/public/script.js
@@ -0,0 +1,15 @@
+fetch("./kismacska/data.json")
+    .then(response => response.json())
+    .then(data => {   
+        for (const beer of data.cards) {
+            document.getElementById('container').insertAdjacentHTML("beforeend",  `
+                    <div class="cards">
+                        <h2>${beer.title}</h2><br>
+                        <p>${beer.sub}</p><br>
+                        <p> ${beer.text}</p><br>
+                    </div>
+                    `            
+            )}
+       
+        }) 
+
