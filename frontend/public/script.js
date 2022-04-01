ac60483339e2d936afb9948fa92e9e713f3fb520
NyergesC <cintia.nyerges@gmail.com>
2022-04-01 23:27:42 +0200

    changes

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -2,8 +2,9 @@ fetch("./kismacska/data.json")
     .then(response => response.json())
     .then(data => {   
         for (const beer of data.cards) {
-            document.getElementById('container').insertAdjacentHTML("beforeend",  `
-                    <div class="cards">
+            document.getElementById('wrapper').insertAdjacentHTML("beforeend",  `
+                    <div class="swiper-slide">
+                        <img src="${beer.img}" alt="hol vagy kocsog?" >
                         <h2>${beer.title}</h2><br>
                         <p>${beer.sub}</p><br>
                         <p> ${beer.text}</p><br>
